264e9b63d87c3051772551dce749d04088c4bca4
Sooraj129 <soorajcrazy2001@gmail.com>
2023-11-13 14:15:45 +0530

    largest-elementsum

--- src/arrays/largestelement.java
+++ src/arrays/largestelement.java
@@ -1,4 +1,18 @@
 package arrays;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class largestelement {
-}
+    public static void main(String[] args) {
+        Scanner  Sc=new Scanner(System.in);
+        int s=Sc.nextInt();
+        int []arrays=new int[s];
+        for (int i = 0; i < s; i++) {
+            arrays[i]=Sc.nextInt();
+        }
+        Arrays.sort(arrays);
+        System.out.println(arrays[arrays.length-1]);
+    }
+    }
+

d3981054929536f6c0b4e82155c306b7fdc05768
Sooraj129 <soorajcrazy2001@gmail.com>
2023-11-13 14:58:20 +0530

    smallestnumberupdate

---
+++ src/arrays/smallestelement.java
@@ -0,0 +1,17 @@
+package arrays;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class smallestelement {
+    public static void main(String[] args) {
+        Scanner Sc = new Scanner(System.in);
+        int s = Sc.nextInt();
+        int[] arrays = new int[s];
+        for (int i = 0; i < s; i++) {
+            arrays[i] = Sc.nextInt();
+        }
+        Arrays.sort(arrays);
+        System.out.println(arrays[0]);
+    }
+}

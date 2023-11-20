56be10e95f0b99133bda00f5380a66188f6eda0c
Sooraj129 <soorajcrazy2001@gmail.com>
2023-11-20 22:39:07 +0530

    frequencyelement

---
+++ src/arrays/frequencyelement.java
@@ -0,0 +1,18 @@
+package arrays;
+
+public class frequencyelement {
+    public static void main(String[] args) {
+        int arr[]={1,1,2,2,4,5};
+        int count=0;
+        for (int i = 0; i < arr.length; i++) {
+
+            for (int j = i+1; j < arr.length ; j++) {
+                if(arr[i]==arr[j]){
+                    count++;
+
+                }
+            }
+        }
+        System.out.println(count);
+    }
+}
